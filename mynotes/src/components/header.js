b26381ddfc9e9705132570ae60e982c7880579ad
HARSH RAJ <122086619+Harshraj843112@users.noreply.github.com>
2025-02-17 13:41:37 +0530

    Update header.js

--- mynotes/src/components/header.js
+++ mynotes/src/components/header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="app-header">
-            <h1>My Notes lATEST  two or three</h1>
+            <h1>My Notes lATEST  two or three 7</h1>
         </div>
     )
 }
